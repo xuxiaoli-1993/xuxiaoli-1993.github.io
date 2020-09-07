cf9a54c2a97a7a5dfbfed55a5148c27a09db1e15
Xuxiao Li <u1013882@utah.edu>
2020-09-06 21:32:58 -0600

    Update 09062020

--- edit_html.sh
+++ edit_html.sh
@@ -3,4 +3,4 @@
 rm -f .*.swp
 rm -f tag
 ctags *
-vim -c "edit index.html|tabe Research.html|tabe software.html|tabe publications.html|tabe style.css|tabe chinese_version.html|tabn"
+vim -c "edit index.html|tabe Research.html|tabe software.html|tabe publications.html|tabe style.css|tabn"
